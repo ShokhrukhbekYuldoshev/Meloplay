6677e179ceb6a48e7777ffb9f7715e751942535f
Shokhrukhbek Yuldoshev <72590392+ShokhrukhbekYuldoshev@users.noreply.github.com>
2024-04-09 19:06:21 +0300

    Update Codemagic configuration

--- lib/src/presentation/pages/player/player_page.dart
+++ lib/src/presentation/pages/player/player_page.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:just_audio_background/just_audio_background.dart';
